f3860d9b005d69319baa81b31a154d1c66338e67
Tom Gilpin <thomasegilpin@gmail.com>
2026-06-04 22:23:25 -0700

    Fix middleware - explicit protected routes only

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,11 @@ export { default } from "next-auth/middleware";
 
 export const config = {
   matcher: [
-    "/((?!$|login|forgot-password|reset-password|api/auth|api/cron|api/forgot-password|api/reset-password|_next/static|_next/image|favicon.ico|icon.png|apple-icon.png|index.html).*)",
+    "/dashboard/:path*",
+    "/bookings/:path*",
+    "/admin/:path*",
+    "/api/bookings/:path*",
+    "/api/admin/:path*",
+    "/api/user/:path*",
   ],
 };
